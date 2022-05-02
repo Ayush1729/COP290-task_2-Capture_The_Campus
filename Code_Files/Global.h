decf92f3335874c4cd804b057f867df704588b45
Ayush Meena <67456054+Ayush1729@users.noreply.github.com>
2022-05-02 22:34:33 +0530

    Update Global.h

--- Code_Files/Global.h
+++ Code_Files/Global.h
@@ -17,7 +17,9 @@ SDL_Renderer* gRenderer = NULL;
 
 
 //Globally used font
-TTF_Font *gFont = NULL;
+TTF_Font *gflappy = NULL;
+TTF_Font *gaerobusdotty = NULL;
+TTF_Font *gPacifico = NULL;
 
 //Current displayed texture
 SDL_Texture* gTexture = NULL;
@@ -33,6 +35,32 @@ SDL_Texture* gTexture_blue_acid = NULL;
 SDL_Texture* gTexture_red_acid = NULL;
 SDL_Texture* gTexture_emp = NULL;
 SDL_Texture* gTexture_shield = NULL;
+SDL_Texture* gSelect_image = NULL;
+SDL_Texture* gTexture_freeze = NULL;
+
+
+SDL_Texture* gTexture_fire_button = NULL;
+SDL_Texture* gTexture_wall_button = NULL;
+SDL_Texture* gTexture_acid_button = NULL;
+SDL_Texture* gTexture_emp_button = NULL;
+SDL_Texture* gTexture_shield_button = NULL;
+SDL_Texture* gTexture_freeze_button = NULL;
+
+
+SDL_Texture* gTexture_instructions_text = NULL; 
+SDL_Texture* gTexture_powers_text = NULL; 
 
 
 SDL_Texture* gTextTexture = NULL;
+
+
+
+//The music that will be played
+Mix_Music *gMusic = NULL;
+Mix_Music *gPlay_Music = NULL;
+
+//The sound effects that will be used
+Mix_Chunk *gbegin_sound = NULL;
+Mix_Chunk *gSelect_sound = NULL;
+Mix_Chunk *gVictory_sound = NULL;
+Mix_Chunk *gLow = NULL;
