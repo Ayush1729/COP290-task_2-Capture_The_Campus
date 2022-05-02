fa6879e03c704e8b6cbfbd9521cc258f89b1cb0b
Ayush Meena <67456054+Ayush1729@users.noreply.github.com>
2022-05-02 22:34:01 +0530

    Update Dependencies.hpp

--- Code_Files/Dependencies.hpp
+++ Code_Files/Dependencies.hpp
@@ -7,19 +7,22 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_net.h>
+#include <SDL2/SDL_mixer.h>
 
 using namespace std;
 
 #include "Global.h"
+#include "load.h"
 #include "Planet.h"
 #include "HomePlanet.h"
 #include "PathPoint.h"
 #include "SoldierPacket.h"
 #include "CannonBall.h"
 #include "States/DoublyLinkedList.h"
-#include "load.h"
 
 
 #include "States/Start_State.h"
 #include "States/Options_State.h"
+#include "States/Powers_State.h"
 #include "States/Play_State.h"
+#include "States/GameOver_State.h"
