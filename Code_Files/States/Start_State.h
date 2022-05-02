fcbfed17e6510d5cc65fb8ebc6aca34e4069d8de
Ayush Meena <67456054+Ayush1729@users.noreply.github.com>
2022-05-02 22:43:34 +0530

    Update Start_State.h

--- Code_Files/States/Start_State.h
+++ Code_Files/States/Start_State.h
@@ -3,28 +3,13 @@ class Start_State{
 	public:
 		Start_State(){
 			//constructor; not necessary
+            Mix_PlayMusic( gMusic, -1 );
 		}
 
-		/*void render(){
-			//
-			int w; int h;
-            SDL_GetWindowSize(gWindow, &w, &h);
-            
-            //SDL_GL_GetDrawableSize(gWindow, &w, &h);
-
-            //Apply the image stretched
-            SDL_Rect stretchRect;
-            stretchRect.x = 0;
-            stretchRect.y = 0;
-            stretchRect.w = w;
-            stretchRect.h = h;
-            SDL_BlitScaled( gBackground, NULL, gScreenSurface, &stretchRect );
-
-            //Update the surface
-            SDL_UpdateWindowSurface( gWindow );
-		}*/
 
 		void update(float dt){
+
+            
 			SDL_Event e;
         	while(SDL_PollEvent( &e ) != 0 ){
                 if( e.type == SDL_QUIT ){
@@ -35,11 +20,13 @@ class Start_State{
                         case SDLK_KP_ENTER:
                         //std::cout<<"keypad side enter only"<<std::endl;
                         gState= "Options";
+                        Mix_PlayChannel( -1, gSelect_sound, 0 );
                         break;
 
                         case SDLK_RETURN:
                         //std::cout<<"main enter pressed"<<std::endl;
                         gState = "Options";
+                        Mix_PlayChannel( -1, gSelect_sound, 0 );
                         break;
 
                         case SDLK_ESCAPE:
@@ -52,10 +39,10 @@ class Start_State{
 
 
                 SDL_Rect title_pos;
-                title_pos.w = 1100*(SCALING_FACTOR_X);
-                title_pos.h = 220*(SCALING_FACTOR_Y);
-                title_pos.x = 100*(SCALING_FACTOR_X);
-                title_pos.y = 100*(SCALING_FACTOR_Y);
+                title_pos.w = (0.9375*SCREEN_WIDTH)*(SCALING_FACTOR_X);
+                title_pos.h = (0.417*SCREEN_HEIGHT)*(SCALING_FACTOR_Y);
+                title_pos.x = (0.039*SCREEN_WIDTH)*(SCALING_FACTOR_X);
+                title_pos.y = (0.139*SCREEN_HEIGHT)*(SCALING_FACTOR_Y);
 
 
                 //Clear screen
@@ -72,49 +59,21 @@ class Start_State{
 
 
 
-                if( gFont == NULL ){
-                    std::cout<<"Failed to load lazy font! SDL_ttf Error: "<< TTF_GetError()<<std::endl;
+                //Render text
+                SDL_Color textColor = { 200, 200, 0 };
+                if( !loadFromRenderedText("Press 'Enter' to Start", textColor, gflappy)) {
+                    std::cout<<"Failed to render text texture!"<<std::endl;
                     error_occ = true;
                 }
-                else
-                {
-                    //Render text
-                    SDL_Color textColor = { 0, 0, 0 };
-                    if( !loadFromRenderedText("The quick brown fox jumps over the lazy dog", textColor ) )
-                    {
-                        std::cout<<"Failed to render text texture!"<<std::endl;
-                        error_occ = true;
-                    }
-                }
-
+            
 
                 SDL_Rect titl_pos;
-                titl_pos.w = 1100*(SCALING_FACTOR_X);
-                titl_pos.h = 220*(SCALING_FACTOR_Y);
-                titl_pos.x = 100*(SCALING_FACTOR_X);
-                titl_pos.y = 350*(SCALING_FACTOR_Y);
+                titl_pos.w = (0.390625*SCREEN_WIDTH)*(SCALING_FACTOR_X);
+                titl_pos.h = (0.07*SCREEN_HEIGHT)*(SCALING_FACTOR_Y);
+                titl_pos.x = (0.3125*SCREEN_WIDTH)*(SCALING_FACTOR_X);
+                titl_pos.y = (0.764*SCREEN_HEIGHT)*(SCALING_FACTOR_Y);
                 // Text on screen
                 SDL_RenderCopy( gRenderer, gTextTexture, NULL, &titl_pos );
-
-
-                if( gFont != NULL ){
-                    //Render text
-                    SDL_Color textColor = { 0, 0, 0 };
-                    if( !loadFromRenderedText("Victory", textColor ) )
-                    {
-                        std::cout<<"Failed to render text texture!"<<std::endl;
-                        error_occ = true;
-                    }
-                }
-
-                SDL_Rect tit_pos;
-                tit_pos.w = 1100*(SCALING_FACTOR_X);
-                tit_pos.h = 220*(SCALING_FACTOR_Y);
-                tit_pos.x = 100*(SCALING_FACTOR_X);
-                tit_pos.y = 600*(SCALING_FACTOR_Y);
-
-                // Text on screen
-                SDL_RenderCopy( gRenderer, gTextTexture, NULL, &tit_pos );
 
 
                 //Update screen
