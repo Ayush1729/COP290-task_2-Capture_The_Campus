25ad1ee592bfbf29e9a9d32264e670fce74a29f0
Ayush Meena <67456054+Ayush1729@users.noreply.github.com>
2022-05-03 11:47:17 +0530

    Update DoublyLinkedList.h

--- Code_Files/States/DoublyLinkedList.h
+++ Code_Files/States/DoublyLinkedList.h
@@ -26,12 +26,6 @@ class DoublyLinkedList{
 			end_sentinel->prev = newPacket;
 		}
 
-		/*void remove(SoldierPacket* newPacket){
-			newPacket->prev->next = newPacket->next;
-			newPacket->next->prev = newPacket->prev;
-			newPacket->next = NULL;
-			newPacket->prev = NULL;
-		}*/
 
 		void update(float dt, Planet* fast_building, Planet* Fire_building, Planet* Acid_building, Planet* Road_block_building, Planet* Freeze_building, Planet* shield_building, Planet* Cannon_building1, Planet* Cannon_building2, CannonBall* cannon_ball_1, CannonBall* cannon_ball_2){
 			SoldierPacket* temp;
@@ -42,11 +36,8 @@ class DoublyLinkedList{
 
 				if ((temp->loyalty != Cannon_building1->loyalty) and (Cannon_building1->power_available) and (cannon_ball_1->alive == false)){
 					if ((pow(temp->x - Cannon_building1->x,2) + pow(temp->y - Cannon_building1->y,2)) < pow(Cannon_building1->radius,2)){
-						//std::cout<<Cannon_building1->power_available<<endl;
 						Cannon_building1->power_available == false;
 						Cannon_building1->power_restore_time = 20;
-
-						std::cout<<"In range: shoot"<<endl;
 
 						cannon_ball_1->set_new(temp);
 					}
